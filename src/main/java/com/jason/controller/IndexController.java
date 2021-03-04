de19c4b8bed71c3befd06863a0dea849eb583446
jason19990512 <jpg74569@gmail.com>
2021-03-04 23:14:22 +0800

    test

---
+++ src/main/java/com/jason/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.jason.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping(value = {"/","/index"})
+    public String index(){
+        return "font-end/index/index";
+    }
+}
